911a07435bc920aaf81b8f2ea7917c7a7bb97331
Aishwaryakemnaik <61688252+Aishwaryakemnaik@users.noreply.github.com>
2021-03-09 20:37:14 +0530

    Add files via upload

---
+++ Assignment Day 1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # QUESTION 1
+
+# In[13]:
+
+
+X = int(input("Enter Value 1: "))
+Y = int(input("Enter Value 2: "))
+print(X**Y)
+
+
+# # QUESTION 2
+
+# In[19]:
+
+
+L = [1,2,3,4,5,6,7,8,9,10,11,12]
+for i in L:
+    if(i%3==0):
+        print(i)
+        
+
+
+# In[ ]:
+
+
+
+
